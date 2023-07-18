f6b5f81154565ba20dd51f04aa3543614c325a88
Leonardoguimaraesdev <leonardo.guimaraes.dev@gmail.com>
2023-07-18 17:31:26 -0300

    Creating humburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,67 @@
-import Image from 'next/image'
-import styles from '../styles/Navbar.module.scss'
-import { useRouter } from 'next/router'
+import Image from "next/image";
+import styles from "../styles/Navbar.module.scss";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function Navbar () {
-    const router = useRouter()
+export default function Navbar() {
+  const router = useRouter();
 
-    const navegation = (e) => {
-        const page = e.target.innerText
+  const [open, setOpen] = useState(false)
 
-        console.log(page)
+  const navegation = (e) => {
+    const page = e.target.innerText;
 
-        if ( page === "HOME") {
-            router.push('/')
-        }
-        if ( page === "SOBRE") {
-            router.push('/about')
-        }
-        if ( page === "CLIENTES") {
-            router.push('/clients')
-        }
-        if ( page === "CONTATO") {
-            router.push('/contact')
-        }
+    console.log(page);
+
+    if (page === "HOME") {
+      router.push("/");
     }
+    if (page === "SOBRE") {
+      router.push("/about");
+    }
+    if (page === "CLIENTES") {
+      router.push("/clients");
+    }
+    if (page === "CONTATO") {
+      router.push("/contact");
+    }
+  };
 
-    return (
-        <nav className={styles.nav}>
-            <div className={styles.left}>
-                <Image src='/logo.png' width='230' height='50' alt='Logo da nibbleone'/>
-            </div>
-            <div className={styles.right}>
-                <ul>
-                    <li onClick={(e) => navegation(e)}>HOME</li>
-                    <li onClick={(e) => navegation(e)}>SOBRE</li>
-                    <li onClick={(e) => navegation(e)}>CLIENTES</li>
-                    <li onClick={(e) => navegation(e)}>CONTATO</li>
-                </ul>
-            </div>
-        </nav>
-    )
+  const showMenu = (e) => {
+    setOpen(!open)
+  }
+
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.left}>
+          <Image
+            src="/logo.png"
+            width="230"
+            height="50"
+            alt="Logo da nibbleone"
+          />
+        </div>
+        <div className={styles.right}>
+          <ul>
+            <li onClick={(e) => navegation(e)}>HOME</li>
+            <li onClick={(e) => navegation(e)}>SOBRE</li>
+            <li onClick={(e) => navegation(e)}>CLIENTES</li>
+            <li onClick={(e) => navegation(e)}>CONTATO</li>
+          </ul>
+        </div>
+        <div className={styles.mobileMenuIcon}>
+          <button onClick={(e) => showMenu(e)}>Menu</button>
+        </div>
+      </nav>
+      <div className={ open ? styles.mobileMenuOpen : styles.mobileMenu}>
+        <ul>
+          <li onClick={(e) => navegation(e)}>HOME</li>
+          <li onClick={(e) => navegation(e)}>SOBRE</li>
+          <li onClick={(e) => navegation(e)}>CLIENTES</li>
+          <li onClick={(e) => navegation(e)}>CONTATO</li>
+        </ul>
+      </div>
+    </>
+  );
 }
